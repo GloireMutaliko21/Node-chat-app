328e07e680ff03248a737110cd89b96e25c25d66
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-16 14:35:27 +0200

    feat: handle change message value

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+    const [message, setMessage] = useState('');
+    const messageLength = message.length < 2;
+
+    const handlerMessage = (e) => {
+        setMessage(e.target.value);
+    };
+
     return (
         <div className='flex flex-col'>
             <textarea
                 placeholder='Votre message'
                 cols={60}
                 className='p-3 border rounded outline-none text-gray-600'
+                value={message}
+                onChange={handlerMessage}
             >
             </textarea>
             <div className='flex justify-end my-5'>
                 <button
                     type='button'
-                    className='bg-sky-600 text-white px-4 py-2 rounded-md'
+                    disabled={messageLength ? true : false}
+                    className={`${!messageLength ? 'bg-sky-600' : 'bg-slate-300'} text-white px-4 py-2 rounded-md`}
                 >
                     Envoyer
                 </button>
