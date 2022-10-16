c23719ae2a0ffd75e2aa2cd6f3b19c7924b2e1c4
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-16 15:19:03 +0200

    style: message style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,9 +3,9 @@ import { BiUserCircle } from "react-icons/bi";
 
 const Message = ({ message }) => {
     return (
-        <div className='flex my-4'>
+        <div className='flex items-center my-4'>
             <p><BiUserCircle className='text-gray-400 text-2xl mr-2' /></p>
-            <p className='bg-sky-500 text-yellow-50 p-2 rounded-t-full rounded-bl-full'>{message}</p>
+            <p className='bg-sky-500 text-yellow-50 p-2 rounded-t-2xl rounded-bl-2xl max-w-xs'>{message}</p>
         </div>
     )
 }
