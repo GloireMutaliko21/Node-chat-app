422a057f6e21e4c831a05d8fed38738fc9c57aab
CREDO23 <thierrybakera12@gmail.com>
2022-10-17 17:33:24 +0200

    feat : pooling

--- api/server.js
+++ api/server.js
@@ -2,13 +2,40 @@ import http from "http";
 import fs from "fs";
 const server = http.createServer();
 server.on("request", (req, res) => {
-fs.readFile('message.txt', (err, data) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  let messages = {};
+  if (req.method === "POST") {
+    const body = [];
+    let allMessages;
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      // console.log(body[0].toString())
+      fs.readFile("message.txt", "utf-8", (err, data) => {
         if (err) console.log("Echec");
-        return res.end(data);
-    })
+        allMessages = JSON.parse(data);
+        messages = JSON.parse(data);
+        let leng = Object.keys(messages).length;
+        messages[leng] = body[0].toString();
+        console.log(messages);
+        fs.writeFile("message.txt", JSON.stringify(messages), (err) => {
+          if (err) console.log("erreur survenue");
+        });
+      });
+    });
+  }
+  fs.readFile("message.txt", (err, data) => {
+    if (err) console.log("Echec");
+    return res.end(data);
+  });
 });
 server.listen(8001);
-
-
-
-
