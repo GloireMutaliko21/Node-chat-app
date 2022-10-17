4c52546101a27c7fcb095d9a9f2ba2daf4a2a34a
jethronbwira@gmail.com <jethronbwira@gmail.com>
2022-10-17 17:21:29 +0200

    feat: refactor create server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
-const http = import('http')
-const server = http.createServer( function(req, res) {
-
+import http from "http";
+import fs from "fs";
+const server = http.createServer();
+server.on("request", (req, res) => {
+fs.readFile('message.txt', (err, data) => {
+        if (err) console.log("Echec");
+        return res.end(data);
+    })
 });
+server.listen(8001);
 
-server.listen(3001);
 
-console.log("server is turning on on 3001")
 
 
