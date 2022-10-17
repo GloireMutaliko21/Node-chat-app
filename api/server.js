cbef0e161ea4891ca8cb2519ee9b4c760b6a816b
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-17 17:19:23 +0200

    feat: init reading file

--- api/server.js
+++ api/server.js
@@ -4,20 +4,11 @@ import fs from "fs";
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    const messages = {}
-    fs.writeFile('message.txt', Buffer.from(JSON.stringify(messages)), (err) => {
-        if (err) console.log("erreur survenue")
-    })
-
     fs.readFile('message.txt', (err, data) => {
         if (err) console.log("Echec");
-        res.end(data);
+        return res.end(data);
     })
-
 
 });
 
-server.listen(8000);
+server.listen(8001);
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,7 +5,7 @@ const Message = ({ message }) => {
     return (
         <div className='flex items-center my-4'>
             <p><BiUserCircle className='text-gray-400 text-2xl mr-2' /></p>
-            <p className='bg-sky-500 text-yellow-50 p-2 rounded-t-2xl rounded-bl-2xl max-w-xs'>{message}</p>
+            <p className='bg-sky-500 text-yellow-50 p-2 rounded-t-2xl rounded-bl-2xl max-w-xs'>{message.slice(1, message.length - 1)}</p>
         </div>
     )
 }
